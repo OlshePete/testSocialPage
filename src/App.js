28a3256f5466a2ad4d086e3dc2f2e69985cdd3bb
@OlshePete <olshevskii.pv@gmail.com>
2021-06-16 14:40:43 +0300

    add route upgrade nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
   return (
-    <div className="app_wrapper">
-      <Header />
-      <Navbar />
-      <div className="content">
-        <Route component={Profile} />
-        <Route component={Dialogs} />
-
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app_wrapper">
+        <Header />
+        <Navbar />
+        <div className="content">
+          <Route path='/profile'  component={Profile} />
+          <Route path='/dialogs'  component={Dialogs} />
+          <Route path='/news'  component={News} />
+          <Route path='/music'  component={Music} />
+          <Route path='/settings'  component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className={s.link}><a href="/dialogs">Messages</a></div>
       <div className={s.link}><a href="/news">News</a></div>
       <div className={s.link}><a href="/music">Music</a></div>
-      <div className={`${s.link} ${s.settings}`}><a src="">Settings</a></div>
+      <div className={`${s.link} ${s.settings}`}><a href="/settings">Settings</a></div>
     </nav>
     )
 }
