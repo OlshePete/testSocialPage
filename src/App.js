77f77c7246022bc9868ff1517ffb1573879e3cbc
@OlshePete <olshevskii.pv@gmail.com>
2021-06-16 14:21:38 +0300

    add dialogs react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <Profile />      
+    <div className="app_wrapper">
+      <Header />
+      <Navbar />
+      <div className="content">
+        <Route component={Profile} />
+        <Route component={Dialogs} />
+
+        <Dialogs />
+      </div>
     </div>
-  );}
+  );
+}
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import s from "./Navbar.module.css"
 const Navbar = () => {
     return (
       <nav className={s.nav}>
-      <div className={s.link}><a src="">Profile</a></div>
-      <div className={s.link}><a src="">Messages</a></div>
-      <div className={s.link}><a src="">News</a></div>
-      <div className={s.link}><a src="">Music</a></div>
+      <div className={s.link}><a href="/profile">Profile</a></div>
+      <div className={s.link}><a href="/dialogs">Messages</a></div>
+      <div className={s.link}><a href="/news">News</a></div>
+      <div className={s.link}><a href="/music">Music</a></div>
       <div className={`${s.link} ${s.settings}`}><a src="">Settings</a></div>
     </nav>
     )
